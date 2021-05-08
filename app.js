21932c4c5da7c9b44fba0ce4677f5ecfe6f314c0
Martin <martinstudyweb@gmail.com>
2021-05-08 19:30:05 +0300

    add semicolom

--- app.js
+++ app.js
@@ -14,27 +14,27 @@
 // push and pop methods for arrays
 // exemple
 const car = [];
-const elem = document.querySelector('#elem')
+const elem = document.querySelector('#elem');
 const message = document.querySelector('.message');
 document.querySelector('.push').onclick = () => {
     let id = +document.querySelector('#goods').value;
     if (!car.includes(id)) {
-        car.push(id)
+        car.push(id);
         elem.textContent += ' ' + id;
         message.textContent = 'You are add new element, good!';
-        setTimeout( () => message.textContent = '', 3000 )
+        setTimeout( () => message.textContent = '', 3000 );
     }
 
 
     console.log(car);
-}
+};
 
 document.querySelector('.pop').onclick = () => {
     let id = +document.querySelector('#goods').value;
     let goods = car.pop();
     elem.textContent = car.join(' ');
     message.textContent = `The item of number id ${goods} was delete`;
-    setTimeout( () => message.textContent = '', 3000 )
-    console.log(`The item of number id ${goods} was delete`)
-    console.log(car)
-}
+    setTimeout( () => message.textContent = '', 3000 );
+    console.log(`The item of number id ${goods} was delete`);
+    console.log(car);
+};
